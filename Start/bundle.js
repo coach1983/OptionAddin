efbff2ae3b5bec5faf72aea2812608f76898151b
NLI-EricRamos <eric.ramos@nlight.net>
2018-05-11 15:57:09 -0700

    excel Tutorial

--- Start/bundle.js
+++ Start/bundle.js
@@ -42,6 +42,154 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/*
+	 * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+	 * See LICENSE in the project root for license information.
+	 */
+
+	'use strict';
+
+	__webpack_require__(1);
+
+	(function () {
+	    Office.initialize = function (reason) {
+	        $(document).ready(function () {
+
+	            if (!Office.context.requirements.isSetSupported('ExcelApi', 1.7)) {
+	                console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
+	            }
+
+	            $('#create-table').click(createTable);
+	            $('#filter-table').click(filterTable);
+	            $('#sort-table').click(sortTable);
+	            $('#create-chart').click(createChart);
+	            $('#freeze-header').click(freezeHeader);
+	            $('#open-dialog').click(openDialog);
+	        });
+	    };
+
+	    function createTable() {
+	        Excel.run(function (context) {
+
+	            const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+	            const expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+	            expensesTable.name = "ExpensesTable";
+
+	            expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
+
+	            expensesTable.rows.add(null /*add at the end*/, [["1/1/2017", "The Phone Company", "Communications", "120"], ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"], ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"], ["1/10/2017", "Coho Vineyard", "Restaurant", "33"], ["1/11/2017", "Bellows College", "Education", "350.1"], ["1/15/2017", "Trey Research", "Other", "135"], ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]]);
+
+	            expensesTable.columns.getItemAt(3).getRange().numberFormat = [['€#,##0.00']];
+	            expensesTable.getRange().format.autofitColumns();
+	            expensesTable.getRange().format.autofitRows();
+
+	            return context.sync();
+	        }).catch(function (error) {
+	            console.log("Error: " + error);
+	            if (error instanceof OfficeExtension.Error) {
+	                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+	            }
+	        });
+	    }
+
+	    function filterTable() {
+	        Excel.run(function (context) {
+
+	            const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+	            const expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+	            const categoryFilter = expensesTable.columns.getItem('Category').filter;
+	            categoryFilter.applyValuesFilter(["Education", "Groceries"]);
+
+	            return context.sync();
+	        }).catch(function (error) {
+	            console.log("Error: " + error);
+	            if (error instanceof OfficeExtension.Error) {
+	                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+	            }
+	        });
+	    }
+
+	    function sortTable() {
+	        Excel.run(function (context) {
+
+	            const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+	            const expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+	            const sortFields = [{
+	                key: 1, // Merchant column
+	                ascending: false
+	            }];
+
+	            expensesTable.sort.apply(sortFields);
+
+	            return context.sync();
+	        }).catch(function (error) {
+	            console.log("Error: " + error);
+	            if (error instanceof OfficeExtension.Error) {
+	                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+	            }
+	        });
+	    }
+
+	    function createChart() {
+	        Excel.run(function (context) {
+
+	            const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+	            const expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+	            const dataRange = expensesTable.getDataBodyRange();
+
+	            let chart = currentWorksheet.charts.add('ColumnClustered', dataRange, 'auto');
+
+	            chart.setPosition("A15", "F30");
+	            chart.title.text = "Expenses";
+	            chart.legend.position = "right";
+	            chart.legend.format.fill.setSolidColor("white");
+	            chart.dataLabels.format.font.size = 15;
+	            chart.dataLabels.format.font.color = "black";
+
+	            return context.sync();
+	        }).catch(function (error) {
+	            console.log("Error: " + error);
+	            if (error instanceof OfficeExtension.Error) {
+	                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+	            }
+	        });
+	    }
+
+	    function freezeHeader() {
+	        Excel.run(function (context) {
+
+	            const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+	            currentWorksheet.freezePanes.freezeRows(1);
+
+	            return context.sync();
+	        }).catch(function (error) {
+	            console.log("Error: " + error);
+	            if (error instanceof OfficeExtension.Error) {
+	                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+	            }
+	        });
+	    }
+
+	    let dialog = null;
+
+	    function openDialog() {
+	        Office.context.ui.displayDialogAsync('https://localhost:3000/popup.html', { height: 35, width: 25 }, function (result) {
+	            dialog = result.value;
+	            dialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogMessageReceived, processMessage);
+	        });
+	    }
+
+	    function processMessage(arg) {
+	        console.log(arg.message);
+	        $('#user-name').text(arg.message);
+	        dialog.close();
+	    }
+	})();
+
+/***/ },
+/* 1 */
 /***/ function(module, exports) {
 
 	/*
@@ -52,120 +200,34 @@
 	'use strict';
 
 	(function () {
+
 	    Office.initialize = function (reason) {
-	        $(document).ready(function () {
 
-	            if (!Office.context.requirements.isSetSupported('ExcelApi', 1.7)) {
-	                console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
-	            }
+	        //If you need to initialize something you can do so here. 
 
-	            formatControls();
-
-	            $("#run").click(run);
-	            $("#createTable").click(createTable);
-	        });
 	    };
-
-	    function formatControls() {
-
-	        var TextFieldElements = document.querySelectorAll(".ms-TextField");
-	        for (var i = 0; i < TextFieldElements.length; i++) {
-	            new fabric['TextField'](TextFieldElements[i]);
-	        }
-
-	        var DropdownHTMLElements = document.querySelectorAll('.ms-Dropdown');
-	        for (var i = 0; i < DropdownHTMLElements.length; ++i) {
-	            var Dropdown = new fabric['Dropdown'](DropdownHTMLElements[i]);
-	        }
-	    }
-
-	    function run() {
-	        Excel.run(function (context) {
-
-	            var range = context.workbook.getSelectedRange();
-	            range.format.fill.color = "yellow";
-	            var s = Number($('#text1').val());
-	            var x = Number($('#text2').val());
-	            var t = Number($('#text3').val());
-	            var r = Number($('#text4').val());
-	            var v = Number($('#text5').val());
-	            var flag = $('#flag').val();
-
-	            var optionVal = Number(BlackScholes(flag, s, x, t, r, v));
-	            optionVal = Number(optionVal.toFixed(2));
-	            var test = addtwoValues(s, x);
-	            console.log(test);
-	            console.log(optionVal);
-
-	            range.values = [[optionVal]];
-
-	            range.load("address");
-	            return context.sync();
-	        }).catch(function (error) {
-	            console.log("Error: " + error);
-	            if (error instanceof OfficeExtension.Error) {
-	                console.log("Debug info: " + JSON.stringify(error.debugInfo));
-	            }
-	        });
-	    }
-
-	    function createTable() {
-	        Excel.run(function (context) {
-
-	            const sheets = context.workbook.worksheets;
-
-	            const sheet = sheets.add();
-	            sheet.name = "Data";
-	            sheets.load("items/name");
-	            context.sync();
-
-	            const currentWorksheet = context.workbook.worksheets.getItem("Data");
-	            const OptionDataTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
-	            OptionDataTable.name = "OptionsDataTable";
-
-	            OptionDataTable.getHeaderRowRange().values = [["Date", "Stock Price", "Strike Price", "Option Value"]];
-
-	            OptionDataTable.rows.add(null /*add at the end*/, [["1/1/2017", "61", "65", "120"], ["1/1/2017", "61", "65", "120"], ["1/1/2017", "61", "65", "120"], ["1/1/2017", "61", "65", "120"], ["1/1/2017", "61", "65", "120"], ["1/1/2017", "61", "65", "120"]]);
-
-	            OptionDataTable.columns.getItemAt(1).getRange().numberFormat = [['$#,##0.00']];
-	            OptionDataTable.columns.getItemAt(2).getRange().numberFormat = [['$#,##0.00']];
-	            OptionDataTable.columns.getItemAt(3).getRange().numberFormat = [['$#,##0.00']];
-	            OptionDataTable.getRange().format.autofitColumns();
-	            OptionDataTable.getRange().format.autofitRows();
-
-	            return context.sync();
-	        }).catch(function (error) {
-	            console.log("Error: " + error);
-	            if (error instanceof OfficeExtension.Error) {
-	                console.log("Debug info: " + JSON.stringify(error.debugInfo));
-	            }
-	        });
-	    }
-
-	    function addtwoValues(a, b) {
-
-	        return a + b;
-	    }
-
-	    function BlackScholes(PutCallFlag, S, X, T, r, v) {
-
-	        var d1, d2;
-	        d1 = (Math.log(S / X) + (r + v * v / 2.0) * T) / (v * Math.sqrt(T));
-	        d2 = d1 - v * Math.sqrt(T);
-
-	        if (PutCallFlag == "c") return S * CND(d1) - X * Math.exp(-r * T) * CND(d2);else return X * Math.exp(-r * T) * CND(-d2) - S * CND(-d1);
-	    }
-
-	    function CND(x) {
-
-	        var a1, a2, a3, a4, a5, k;
-
-	        a1 = 0.31938153, a2 = -0.356563782, a3 = 1.781477937, a4 = -1.821255978, a5 = 1.330274429;
-
-	        if (x < 0.0) return 1 - CND(-x);else k = 1.0 / (1.0 + 0.2316419 * x);
-	        return 1.0 - Math.exp(-x * x / 2.0) / Math.sqrt(2 * Math.PI) * k * (a1 + k * (-0.356563782 + k * (1.781477937 + k * (-1.821255978 + k * 1.330274429))));
-	    }
 	})();
+
+	function toggleProtection(args) {
+	    Excel.run(function (context) {
+	        const sheet = context.workbook.worksheets.getActiveWorksheet();
+	        sheet.load('protection/protected');
+
+	        return context.sync().then(function () {
+	            if (sheet.protection.protected) {
+	                sheet.protection.unprotect();
+	            } else {
+	                sheet.protection.protect();
+	            }
+	        }).then(context.sync);
+	    }).catch(function (error) {
+	        console.log("Error: " + error);
+	        if (error instanceof OfficeExtension.Error) {
+	            console.log("Debug info: " + JSON.stringify(error.debugInfo));
+	        }
+	    });
+	    args.completed();
+	}
 
 /***/ }
 /******/ ]);
